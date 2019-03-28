9277c168d2da37c22cefa9b0e8946d5c7235a38a
tanmayb123 <tajymany@gmail.com>
2019-03-28 16:08:42 -0400

    Add Segue examples

---
+++ 1_Chapter02_Segues_Extra/1_Chapter02_Segues_Extra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1_Chapter02_Segues_Extra
+//
+//  Created by Tanmay Bakshi on 2019-03-28.
+//  Copyright © 2019 Tanmay Bakshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
